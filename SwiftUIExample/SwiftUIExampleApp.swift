73904692cb2dfe0ddeff1692e68fd4b63202797b
Eli Sokelalnd <elisokeland@gmail.com>
2022-10-13 22:16:55 -0400

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Eli Sokeland on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
